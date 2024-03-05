525e67f6e3d86c5b179046649aba954207757149
Mykyta Fediaiev <Nick.Fediaiev@gmail.com>
2024-03-05 20:25:38 +0200

    minor reimport

--- Runtime/ScreenManagerModule/System/Installers/ScreensManagerInstaller.cs
+++ Runtime/ScreenManagerModule/System/Installers/ScreensManagerInstaller.cs
@@ -1,18 +1,28 @@
-﻿using ScreenManagerModule.System.Components;
+﻿using GameCoreModule.Interface.Installers;
+using ScreenManagerModule.System.Components;
 using ScreenManagerModule.System.Managers;
 using UnityEngine;
 using Zenject;
 namespace ScreenManagerModule.System.Installers
 {
-    public class ScreensManagerGlobalInstaller : MonoInstaller
+    public class ScreensManagerInstaller : ModuleInstallerBase<ScreensManagerLocalInstaller>
     {
         [SerializeField] private RootScreensCanvas rootScreensCanvas;
-        
+
+        protected override string SubContainerName => "ScreensManager";
         public override void InstallBindings()
         {
             Container.BindInterfacesAndSelfTo<ScreensManager>().AsSingle();
             
             Container.Bind<RootScreensCanvas>().FromComponentInNewPrefab(rootScreensCanvas).AsSingle();
+        }
+    }
+
+    public class ScreensManagerLocalInstaller : Installer
+    {
+        public override void InstallBindings()
+        {
+            
         }
     }
 }

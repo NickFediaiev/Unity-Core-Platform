40b5a1c068e893c9af277b250742114bf4e96b47
Mykyta Fediaiev <Nick.Fediaiev@gmail.com>
2024-03-21 17:02:27 +0200

    removed redundant event

--- Runtime/MultiplayerModule/System/Components/PlayerSpawnEventInvoker.cs
+++ Runtime/MultiplayerModule/System/Components/PlayerSpawnEventInvoker.cs
@@ -1,12 +1,9 @@
-using System;
 using FishNet.Object;
 using MultiplayerModule.Interface.Components;
 namespace MultiplayerModule.System.Components
 {
     public class PlayerSpawnEventInvoker : NetworkBehaviour
     {
-        public static event Action OnPlayerSpawn;
-    
         public override void OnStartClient()
         {
             base.OnStartClient();
